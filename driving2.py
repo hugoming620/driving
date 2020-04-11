d189874690971c71da7063c1aa223ee605560dc5
Mac <mac@Macde-MacBook-Pro.local>
2020-04-11 09:17:08 +0800

    add usa part

--- driving2.py
+++ driving2.py
@@ -1,6 +1,5 @@
-#driving 2
+#driving 2 (add new usa)
 country = input('你的國家是？ ')
-
 if country == 'taiwan' :
 	age = input('請輸入年齡？ ')
 	age = int(age)
@@ -8,6 +7,13 @@ if country == 'taiwan' :
 		print('你已經',age,'歲了，可以考駕照')
 	else : 
 		print('you`re not allowed to get liance!')
+elif country == 'usa' :
+	age = input('請輸入年齡？ ')
+	age = int(age)
+	if age >= 16 :
+		print('你已經',age,'歲了，可以考駕照')
+
 else :
-	print('其他不知道！只知道台灣')
+	print(' 其他不知道！只知道taiwan and usa :> ')
+
 	
